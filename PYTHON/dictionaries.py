b92a90566a9e2169b940e2d2f7b44511c5ac3931
joseortiz1080 <joseortiz1080@gmail.com>
2024-09-06 14:44:26 -0500

    Agrego Dictionaries.py

---
+++ PYTHON/dictionaries.py
@@ -0,0 +1,19 @@
+#   crear un diccionario en donde se le asigne un valor a cada llave, ejmp: Harry - gryffindor y Draco - Slydering
+
+
+houses = {"Harry":"Griffindor", "Draco":"Slydering"}
+
+# ahora si quiero saber en que casa esta Harry, realizo lo siguiente:
+
+print (houses["Harry"])
+
+
+# si quiero agregar una llave con un valor podria hacerlo de la siguiente forma:
+
+houses ["Hermione"] = "Griffindor"
+
+# aqui imprimimos ahora el valor asignado a la llave Hermione
+
+print(houses["Hermione"])
+
+
