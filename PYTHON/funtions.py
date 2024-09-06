302d243bbf2f0824756649585e19a24c84690be7
joseortiz1080 <joseortiz1080@gmail.com>
2024-09-06 17:34:55 -0500

    Agregue funtios.py

---
+++ PYTHON/funtions.py
@@ -0,0 +1,11 @@
+# Con las funciones hay algunas predeterminadas, como por ejemplo input, print, etc, pero nosotros podemos crear una funcion tambien
+
+# vamos a crear una funcion para definir los lados de un cuadrado, para definir funciones utilizamos el parametro def
+# Esta función coge un rango de 10, y lo utiliza para hacer 10 iteraciones y en cada iteración ejecuta la función square, la cual 
+# remplaza el numero de iteración en la variable i y luego remplaza la variable x de la función y la multiplica por el numero en i
+
+def square (x):
+    return x*x
+
+for i in range (10):
+    print(f"The square of {i} is {square (i)}")
