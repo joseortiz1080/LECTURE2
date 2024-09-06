6884f62ec62557259fb9fbd3b35f5a1d399245f1
joseortiz1080 <joseortiz1080@gmail.com>
2024-09-06 13:57:37 -0500

    Agrego ejemplos de lists

---
+++ PYTHON/lists.py
@@ -0,0 +1,12 @@
+# Define a list of names
+names = ["Harry", "Ron", "Hermione", "Ginny"]
+
+print(names)
+
+# Agrega el valor Draco a la lista antes creada
+names.append ("Draco")
+
+# Modfica el orden de la lista en orden desendente 
+names.sort()
+
+print(names)
