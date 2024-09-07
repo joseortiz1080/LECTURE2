c15407fa48c228225a80166b34a7c080936e34c9
joseortiz1080 <joseortiz1080@gmail.com>
2024-09-07 15:39:45 -0500

    se ajustan los comentarios

--- PYTHON/funtions.py
+++ PYTHON/funtions.py
@@ -1,6 +1,6 @@
 # Con las funciones hay algunas predeterminadas, como por ejemplo input, print, etc, pero nosotros podemos crear una funcion tambien
 
-# vamos a crear una funcion para definir los lados de un cuadrado, para definir funciones utilizamos el parametro def
+# vamos a crear una funcion para definir la raiz de los numeros del 1 al 9, para definir funciones utilizamos el parametro def
 # Esta función coge un rango de 10, y lo utiliza para hacer 10 iteraciones y en cada iteración ejecuta la función square, la cual 
 # remplaza el numero de iteración en la variable i y luego remplaza la variable x de la función y la multiplica por el numero en i
 
